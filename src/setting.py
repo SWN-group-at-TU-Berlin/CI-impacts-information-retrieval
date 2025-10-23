aa155ae2af965b43f3dc42b3822150dfbced5954
A-Buch <anna.buch@tu-berlin.de>
2025-10-23 16:40:56 +0200

    build: set folderpaths, init src and test folders

---
+++ src/setting.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Global variables and logger functions"""
+
+__author__ = "Anna Buch, TU Berlin"
+__email__ = "anna.buch@tu-berlin.de"
+
+
+
+# src paths
+PATH_SRC = "./src"
+# log path
+PATH_LOGS = "../logs/"
+# define input data paths
+PATH_DATA = "../data/"
+
+
