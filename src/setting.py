28a3b273e1e5e097861a6fee73443a6297ce7a41
A-Buch <anna.buch@tu-berlin.de>
2025-10-27 21:08:46 +0100

    build(env,settings): add packages and redefine use of global settings

--- src/setting.py
+++ src/setting.py
@@ -5,13 +5,23 @@
 __author__ = "Anna Buch, TU Berlin"
 __email__ = "anna.buch@tu-berlin.de"
 
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-# src paths
-PATH_SRC = "./src"
-# log path
-PATH_LOGS = "../logs/"
-# define input data paths
-PATH_DATA = "../data/"
 
 
+class Settings(BaseSettings):
+
+    # paths
+    PATH_SRC:str = "./src"
+    ## store logs and data outside of the repository
+    PATH_LOGS:str = "../logs/"
+    PATH_DATA:str = "../data/"
+
+    HUGGINGFACE_API_URL: str = "https://api-inference.huggingface.co/models/meta-llama/Meta-Llama-3-8B-Instruct"
+    HUGGINGFACE_TOKEN: str
+    model_config = SettingsConfigDict(env_file='.env') # load HUGGINGFACE_TOKEN
+
+
+
+settings = Settings()
