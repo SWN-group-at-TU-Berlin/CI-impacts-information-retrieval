999b9722c6659f4a0cf9b1ebdac2772fef9f2880
a-buch <anna.buch@tu-berlin.de>
2025-11-21 00:41:48 +0100

    build/refactor: updated env, rename settings.py

--- src/settings.py
+++ src/settings.py
@@ -8,20 +8,18 @@ __email__ = "anna.buch@tu-berlin.de"
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-
-
 class Settings(BaseSettings):
-
     # paths
-    PATH_SRC:str = "./src"
+    PATH_SRC: str = "./src"
     ## store logs and data outside of the repository
-    PATH_LOGS:str = "../logs/"
-    PATH_DATA:str = "../data/"
+    PATH_LOGS: str = "../logs/"
+    PATH_DATA: str = "../data/"
 
-    HUGGINGFACE_API_URL: str = "https://api-inference.huggingface.co/models/meta-llama/Meta-Llama-3-8B-Instruct"
     HUGGINGFACE_TOKEN: str
-    model_config = SettingsConfigDict(env_file='.env') # load HUGGINGFACE_TOKEN
+    model_config = SettingsConfigDict(env_file=".env")  # load HUGGINGFACE_TOKEN
 
+    CHUNK_SIZE: int = 512
+    CHUNK_OVERLAP: int = 50
 
 
 settings = Settings()
