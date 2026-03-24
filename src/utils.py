000599587de0a811e3f701ea080ddc5f4c045a47
a-buch <anna.buch@tu-berlin.de>
2026-03-24 22:16:04 +0100

    feat(eval): add func for vector_calculation

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,43 @@
-import numpy as np
+import gc
 
-def cosine_similarity(vector_a, vector_b):
-    dot_product = np.dot(vector_a, vector_b)
-    magnitude_a = np.linalg.norm(vector_a)
-    magnitude_b = np.linalg.norm(vector_b)
-    return dot_product / (magnitude_a * magnitude_b)
+
+import numpy as np
+import torch
+from sentence_transformers import SentenceTransformer
+from sentence_transformers.util import cos_sim
+
+from src.settings import settings as s
+
+
+def vector_calculation(token_pred:str, token_valid:str) -> np.empty(shape=(2,)):
+
+    ## Comparison different methods to calc cosine similarity
+    # NOTE when using en_core_web_trf --> better use sentence_transformer which also accounts for context within multi-word LLM responses
+    # as proposed by spaCy developers: https://github.com/explosion/spaCy/discussions/10361
+    # while en_core_web_lg can only be used for word-level similarity,
+
+    ## Example
+    ## see the differences in the cos. similarity scores depending on how the vectors were calculated:
+    # cos. similarity:  ["aviation","air traffic"] # _trf: tensor([[0.6789]])  (<-> compared to 0.4534 with spacy en_core_web_lg)
+    # cos. similarity: ["transportation","transport infrastructure"]  # _trf: tensor([[0.7618]]) (<-> compared to 0.7564 with spacy en_core_web_lg)
+    
+    # infer device and clean up GPU memory
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    gc.collect()
+    torch.cuda.empty_cache() 
+    torch.no_grad()
+    
+    # get contextual vectors
+    model = SentenceTransformer('all-MiniLM-L6-v2', device=device)
+    sentences = [token_pred, token_valid]
+    embedded_list = model.encode(sentences, device=device, batch_size=5)
+
+    return embedded_list
+
+
+def cosine_similarity(vec_a:np.array, vec_b:np.array) -> float:
+    """Calculate cosine similarity for contextual vectors"""
+    similarity = cos_sim(vec_a, vec_b)
+    
+    return similarity.item()
 
