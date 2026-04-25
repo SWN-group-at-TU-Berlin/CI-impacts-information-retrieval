bc24bc6a8eabd7da525d1b137fe8c72ff0b53b64
A-Buch <anna.buch@tu-berlin.de>
2026-04-26 01:33:32 +0200

    fix/feat(postp): add handling of multi outputs (dam_1->dam), add handling of faulty llm responses with [[

--- src/postprocess.py
+++ src/postprocess.py
@@ -5,7 +5,7 @@
 __author__ = "Anna Buch, TU Berlin"
 __email__ = "anna.buch@tu-berlin.de"
 
-
+import re
 from io import StringIO
 import pandas as pd
 
@@ -36,6 +36,7 @@ def group_ci_types(df: pd.DataFrame, col_type, col_grouped, ci_patterns: pd.Data
         subgroup = r[pattern]
     
         # assign subgroups to CI records, na=False to remove all records which not match patterns
+        df[col_type] = df[col_type].astype(str)
         mask = df[col_type].str.contains(pattern, regex=True, na=False)
         df.loc[mask, col_grouped] = subgroup
     
@@ -44,7 +45,10 @@ def group_ci_types(df: pd.DataFrame, col_type, col_grouped, ci_patterns: pd.Data
 
 def postprocess_response(resp: str) -> pd.DataFrame:
 
+    # Remove linebreak symbols and case numbers in the keys ("damage_1" -> "damage")
     resp = resp.replace("\n", "")
+    resp = re.sub(pattern=r"_\d+", repl="", string=resp)
+
     # fix missing bracket at beginning and end of response by splitting at first/last complete entry
     if "[{" not in resp:
         resp = "[{" + resp.split("{", 1)[1]
@@ -52,10 +56,18 @@ def postprocess_response(resp: str) -> pd.DataFrame:
         resp = resp.rpartition('}')[-3] + "}]"
 
     # remove potential text outside - no matter if it exists or not
-    resp = ("[" + resp.split("[")[1]) 
-    resp = (resp.split("]", 1)[0] + "]") 
+    final_resp = ("[" + resp.split("[")[1]) 
+    final_resp = (final_resp.split("]", 1)[0] + "]") 
 
-    df_resp = pd.read_json(StringIO(resp))
-    
-    return df_resp
+    # check for empty response due to removal of text outside of brackets
+    if final_resp == "[]" or final_resp == "[{}]":
+        # remove potential double [[ and ]]
+        final_resp = resp.replace("[[{", "[{").replace("}]]", "}]")
+        # remove potential text outside - no matter if it exists or not
+        final_resp = ("[{" + final_resp.split("[{")[1]) 
+        final_resp = (final_resp.split("}]", 1)[0] + "}]") 
+
+    df_final_resp = pd.read_json(StringIO(final_resp))
+
+    return df_final_resp
         
