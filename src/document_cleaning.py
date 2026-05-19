b030820dd49113aee9ab0e723e21b7399a2cb18f
A-Buch <anna.buch@tu-berlin.de>
2026-05-19 16:33:17 +0200

    interim: including docling.parser into  doc.cleaning WF,also create own DoclingDoc to store cleaned texts aling with meta data

--- src/document_cleaning.py
+++ src/document_cleaning.py
@@ -5,14 +5,52 @@
 __author__ = "Anna Buch, TU Berlin"
 __email__ = "anna.buch@tu-berlin.de"
 
-from contextlib import suppress
+from pathlib import Path
 import re
 import warnings
-from docling_core.types.doc.document import TextItem
-from docling.document_converter import ConversionResult
-from langchain_core.documents import Document
+from contextlib import suppress
+
 import json
 from typing import Iterable
+from docling_core.types.doc.document import TextItem
+from langchain_core.documents import Document
+from docling.datamodel.pipeline_options import PdfPipelineOptions, EasyOcrOptions, AcceleratorOptions
+from docling.document_converter import ConversionResult, DocumentConverter, PdfFormatOption, PipelineOptions, InputFormat
+
+
+
+
+class DocumentParser:
+    """Class for parsing and cleaning documents."""
+
+    def __init__(self):
+                
+        # OCR pipeline configs
+        self.artifacts_path = Path("./docling_artifacts")
+        self.artifacts_path.mkdir(exist_ok=True)
+
+        # OCR and pipeline options
+        ocr_options = EasyOcrOptions(
+            lang=["en", "fr", "de", "es", "it", "nl"],
+            download_enabled=True
+        )
+        pipeline_opts = PdfPipelineOptions(
+            artifacts_path=self.artifacts_path,
+            do_ocr=True,         # Required for text extraction
+            do_table_structure=False,  # Disable table analysis if not needed
+            ocr_options=ocr_options
+        )
+        # PDF format options
+        pdf_format_option = PdfFormatOption(
+            pipeline_options=pipeline_opts,
+            reading_order="natural"
+        )
+        # init pdf converter
+        self.ocr_converter = DocumentConverter(
+            format_options={InputFormat.PDF: pdf_format_option}
+        )
+
+
 
 def extract_citation_info(citation_text: str) -> str:
     """Extract citation information from the document title (i.e. citation id)."""
@@ -56,38 +94,38 @@ def remove_references(document_text: str) -> str:
         return document_text
 
 
-# def remove_headers_footers(conv_file: ConversionResult) -> ConversionResult:
-#     ## remove headers and footers from the document
-#     total_texts = len(conv_file.document.texts)
-#     print(f"Total texts in document: {total_texts}")
+def remove_headers_footers(conv_file: ConversionResult) -> ConversionResult:
+    ## remove headers and footers from the document
+    total_texts = len(conv_file.document.texts)
+    print(f"Total texts in document: {total_texts}")
 
-#     text_items = [x for x in conv_file.document.texts if isinstance(x, TextItem)]
+    text_items = [x for x in conv_file.document.texts if isinstance(x, TextItem)]
 
-#     text_items_to_drop = []
-#     text_items_to_drop_visualization = []
+    text_items_to_drop = []
+    text_items_to_drop_visualization = []
 
-#     ## select text items to drop based on their number of chars, e.g. headers/footers, text in figures
-#     for i in text_items:
-#         # FIXME still removes some subsection headers due that they arent tagged as SECTION_HEADER
-#         # each of the conditions has a drawback
-#         #   char threshold removes some subsection tiles thus apply it not for text_items marked as SECTION_HEADER,
-#         #   "BODY" includes also words in images,
-#         # IDEA check intermediate markdown (Korzilius, Mohr) if subsection headers are rendered by \n\n <subsection header> \n or similarly
-#         if (
-#             i.content_layer.name == "BODY"
-#             and len(i.text) < 50
-#             and i.label.name != "SECTION_HEADER"
-#         ):
-#             text_items_to_drop.append(i)
-#             text_items_to_drop_visualization.append([len(i.text), i.text])
+    ## select text items to drop based on their number of chars, e.g. headers/footers, text in figures
+    for i in text_items:
+        # FIXME still removes some subsection headers due that they arent tagged as SECTION_HEADER
+        # each of the conditions has a drawback
+        #   char threshold removes some subsection tiles thus apply it not for text_items marked as SECTION_HEADER,
+        #   "BODY" includes also words in images,
+        # IDEA check intermediate markdown (Korzilius, Mohr) if subsection headers are rendered by \n\n <subsection header> \n or similarly
+        if (
+            i.content_layer.name == "BODY"
+            and len(i.text) < 50
+            and i.label.name != "SECTION_HEADER"
+        ):
+            text_items_to_drop.append(i)
+            text_items_to_drop_visualization.append([len(i.text), i.text, i])
 
-#     ## drop selected text items
-#     conv_file.document.delete_items(node_items=text_items_to_drop)
+    ## drop selected text items
+    conv_file.document.delete_items(node_items=text_items_to_drop)
 
-#     texts_cleaned = len(conv_file.document.texts)
-#     print(f"Total texts after deletion: {texts_cleaned}")
+    print(f"Total texts after deletion: {len(conv_file.document.texts)}")
 
-#     return conv_file
+    return conv_file, text_items_to_drop_visualization
+
 
 def save_doc_to_jsonl(array:Iterable[Document], file_path:str)->None:
     """ Saving langchain for DocumentLoader to jsonl"""
