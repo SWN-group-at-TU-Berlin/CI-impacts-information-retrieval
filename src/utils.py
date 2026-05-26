24939c65cb7c0f8a92ec085b8bcae3f86a5b535d
A-Buch <anna.buch@tu-berlin.de>
2026-05-26 18:32:55 +0200

    perf/feat/fix(eval): multiple improvements
    * perf: speedup semantic similarity calculation (embedding vector creation) by calling embedding modle just once
    
    * feat: cleanup texts, document-wise evaluation, geolocalization improvement

--- src/utils.py
+++ src/utils.py
@@ -23,43 +23,45 @@ def gen_dict_extract(var, key):
             yield from gen_dict_extract(d, key)
 
 
-def vector_calculation(token_pred:str, token_valid:str) -> np.empty(shape=(2,)):
+class EmbeddingModel:
 
-    ## Comparison different methods to calc cosine similarity
-    # NOTE when using en_core_web_trf --> better use sentence_transformer which also accounts for context within multi-word LLM responses
-    # as proposed by spaCy developers: https://github.com/explosion/spaCy/discussions/10361
-    # while en_core_web_lg can only be used for word-level similarity,
+    def __init__(self):
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model = SentenceTransformer('all-MiniLM-L6-v2', device=device)
+                
+    def vector_calculation(self, token_pred:str, token_valid:str) -> np.empty(shape=(2,)):
 
-    ## Example
-    ## see the differences in the cos. similarity scores depending on how the vectors were calculated:
-    # cos. similarity:  ["aviation","air traffic"] # _trf: tensor([[0.6789]])  (<-> compared to 0.4534 with spacy en_core_web_lg)
-    # cos. similarity: ["transportation","transport infrastructure"]  # _trf: tensor([[0.7618]]) (<-> compared to 0.7564 with spacy en_core_web_lg)
+        ## Comparison different methods to calc cosine similarity
+        # NOTE when using en_core_web_trf --> better use sentence_transformer which also accounts for context within multi-word LLM responses
+        # as proposed by spaCy developers: https://github.com/explosion/spaCy/discussions/10361
+        # while en_core_web_lg can only be used for word-level similarity,
+
+        ## Example
+        ## see the differences in the cos. similarity scores depending on how the vectors were calculated:
+        # cos. similarity:  ["aviation","air traffic"] # _trf: tensor([[0.6789]])  (<-> compared to 0.4534 with spacy en_core_web_lg)
+        # cos. similarity: ["transportation","transport infrastructure"]  # _trf: tensor([[0.7618]]) (<-> compared to 0.7564 with spacy en_core_web_lg)
+        
+        # clean up GPU memory
+        gc.collect()
+        torch.cuda.empty_cache() 
+        torch.no_grad()
+        
+        # get contextual vectors
+        sentences = [token_pred, token_valid]
+        embedded_list = self.model.encode(sentences, device=self.model.device, batch_size=5)
+
+        return embedded_list
     
-    # infer device and clean up GPU memory
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    gc.collect()
-    torch.cuda.empty_cache() 
-    torch.no_grad()
-    
-    # get contextual vectors
-    model = SentenceTransformer('all-MiniLM-L6-v2', device=device)
-    sentences = [token_pred, token_valid]
-    embedded_list = model.encode(sentences, device=device, batch_size=5)
 
-    return embedded_list
-
-
-def cosine_similarity(vec_a:np.array, vec_b:np.array) -> float:
-    """Calculate cosine similarity for contextual vectors"""
-    similarity = cos_sim(vec_a, vec_b)
-    
-    return similarity.item()
+    def cosine_similarity(self, vec_a:np.array, vec_b:np.array) -> float:
+        """Calculate cosine similarity for contextual vectors"""
+        similarity = cos_sim(vec_a, vec_b)
+        return similarity.item()
 
 
 
-
-def calc_recall(tps_no: int, fps_no: int, fns_no: int):
-    return tps_no / (fps_no + fns_no) 
+def calc_recall(tps_no: int, fns_no: int):
+    return tps_no / (tps_no + fns_no) 
 
 
 
